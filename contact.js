909e6b5ae3da103a706dd516ed2c5962585088f5
Sam Pomajevich <47705880+FSP1020@users.noreply.github.com>
2021-04-05 22:11:47 -0500

    Js

--- contact.js
+++ contact.js
@@ -2,13 +2,14 @@
 
 function verify()
 {
-	var fname = getElementById("fname").value;
-	var lname = getElementById("lname").value;
-	var email = getElementById("email").value;
+	alert("Hello")
+	// var fname = getElementById("fname").value;
+	// var lname = getElementById("lname").value;
+	// var email = getElementById("email").value;
 
-	if ((fname == "") || (lname == "") || (email == ""))
-	{
-		alert("Please fill in all fields!");
-	}
+	// if ((fname == "") || (lname == "") || (email == ""))
+	// {
+	// 	alert("Please fill in all fields!");
+	// }
 
 }

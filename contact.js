db13f7429f39e1d288ce048a64d5b8a783e49451
Sam Pomajevich <47705880+FSP1020@users.noreply.github.com>
2021-04-05 21:50:23 -0500

    JS Feedback

---
+++ contact.js
@@ -0,0 +1,12 @@
+function verify()
+{
+	var fname = getElementById("fname");
+	var lname = getElementById("lname");
+	var email = getElementById("email");
+
+	if ((fname == "") || (lname == "") || (email == ""))
+	{
+		alert("Empty field, please fill all fields!");
+	}
+
+}

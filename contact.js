8dac7c7458b07249133b0842adba5ca4c0b81984
Sam Pomajevich <47705880+FSP1020@users.noreply.github.com>
2021-04-05 23:01:30 -0500

    fixed js

--- contact.js
+++ contact.js
@@ -5,12 +5,15 @@ function verify()
 	var fname = document.getElementById("fname").value;
 	var lname = document.getElementById("lname").value;
 	var email = document.getElementById("email").value;
-	var feedback = document.getElementById("feedback").innerText;
+	var feedback = document.getElementById("feedback").value.trim();
 
 
 	if ((fname == "") || (lname == "") || (email == "") || (feedback == ""))
 	{
 		alert("Please fill in all fields!")
+	}
+	else {
+		document.getElementById("aForm").reset();
 	}
 
 }
